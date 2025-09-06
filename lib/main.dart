11824711dde52b9f77f940b755aabfbe5ab7e60e
Veronica Matore <nicamatore0@gmail.com>
2025-09-06 21:17:58 +0800

    Eme

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,21 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
-<<<<<<< HEAD
-import 'navBar.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  try {
-    await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: "AIzaSyBcWBAJ2lCy9wOFx-kONqoxkTO9ey0E9Us",
-        authDomain: "helmet-858a5.firebaseapp.com",
-        databaseURL: "https://helmet-858a5-default-rtdb.firebaseio.com",
-        projectId: "helmet-858a5",
-        storageBucket: "helmet-858a5.firebasestorage.app",
-        messagingSenderId: "1013766552572",
-        appId: "1:1013766552572:web:6887e6500b7e8a18da5079",
-        measurementId: "G-VCMY69XB2P",
-      ),
-    );
-  } catch (e) {
-    print("Firebase initialization error: $e");
-  }
-
-  runApp(MyApp());
-=======
 import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 import 'navBar.dart';
 import 'signin.dart';
 import 'signup.dart';
 import 'home.dart';
 
-const firebaseWebOptions = FirebaseOptions(
-  apiKey: "AIzaSyBcWBAJ2lCy9wOFx-kONqoxkTO9ey0E9Us",
-  authDomain: "helmet-858a5.firebaseapp.com",
-  databaseURL: "https://helmet-858a5-default-rtdb.firebaseio.com",
-  projectId: "helmet-858a5",
-  storageBucket: "helmet-858a5.firebasestorage.app",
-  messagingSenderId: "1013766552572",
-  appId: "1:1013766552572:web:6887e6500b7efl8a18da5079",
-  measurementId: "G-VCMY69XB2P",
-);
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (kIsWeb) {
-    await Firebase.initializeApp(options: firebaseWebOptions);
-  } else {
-    await Firebase.initializeApp();
-  }
+  // Use the auto-generated options for all platforms
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
->>>>>>> main
 }
 
 class MyApp extends StatelessWidget {
@@ -65,23 +25,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      title: 'Topshield',
-      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
-      home: const BottomNavBar(),
-=======
       title: 'My App',
       theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
 
-      // 👇 Listen to FirebaseAuth state
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator()); // loading screen
+            return const Center(child: CircularProgressIndicator());
           }
           if (snapshot.hasData) {
-            return HomePage(); // already logged in
+            return const HomePage(); // user logged in
           }
           return SignIn(); // not logged in
         },
@@ -92,7 +46,6 @@ class MyApp extends StatelessWidget {
         '/signin': (context) => SignIn(),
         '/signup': (context) => SignUp(),
       },
->>>>>>> main
     );
   }
 }
